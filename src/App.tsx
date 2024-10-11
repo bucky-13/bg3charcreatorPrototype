472f0d36a3b557d410f77f66ad2ff889b80f5895
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-10-11 10:30:05 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,13 @@ function App() {
   return (
     <>
       <div>
-        <h2>{abilities[0].name}</h2>
-        <img src={abilities[2].iconSrc}></img>
-        <p>{abilities[0].desc}</p>
-        <img src="/icons/abilities/str_icon.png"></img>
-        <img src="../icons/abilities/str_icon.png"></img>
-        <img src="./icons/abilities/str_icon.png"></img>
-        <img src="/icons/abilities/str_icon.png"></img>
-        <img src="/bg3charcreatorPrototype/icons/abilities/str_icon.png"></img>
+        {abilities.map((ability) => (
+          <div>
+            <h2>{ability.name}</h2>
+            <img src={ability.iconSrc}></img>
+            <p>{ability.desc}</p>
+          </div>
+        ))}
       </div>
     </>
   );
