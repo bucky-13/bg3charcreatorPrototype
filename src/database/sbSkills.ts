171500472718daa57fc0d5edb2e234e04b8013b4
Simon Stenström <simon.stenstrom.013@gmail.com>
2024-10-11 10:53:47 +0200

    added skills db

---
+++ src/database/sbSkills.ts
@@ -0,0 +1,112 @@
+import { ISkill } from '../models/ISkill';
+
+export const skills: ISkill[] = [
+  {
+    skillId: 'ski01',
+    name: 'Athletics',
+    desc: 'Stay fit. Perform physical stunts',
+    parentId: 'abi01',
+  },
+  {
+    skillId: 'ski02',
+    name: 'Acrobatics',
+    desc: 'Keep your balance. Land on your feet',
+    parentId: 'abi02',
+  },
+  {
+    skillId: 'ski03',
+    name: 'Sleight of Hand',
+    desc: 'Wield nimble fingers. Steal stuff',
+    parentId: 'abi02',
+  },
+  {
+    skillId: 'ski04',
+    name: 'Stealth',
+    desc: 'Stay out of sight. Melt into the shadows.',
+    parentId: 'abi02',
+  },
+  {
+    skillId: 'ski05',
+    name: 'Arcana',
+    desc: 'Recognise magic. Interact with enchanted items.',
+    parentId: 'abi04',
+  },
+  {
+    skillId: 'ski06',
+    name: 'History',
+    desc: 'Remember the past -- of the world and its people.',
+    parentId: 'abi04',
+  },
+  {
+    skillId: 'ski07',
+    name: 'Investigation',
+    desc: 'Analyse clues. Solve mysteries.',
+    parentId: 'abi04',
+  },
+  {
+    skillId: 'ski08',
+    name: 'Nature',
+    desc: 'Recognise plants and animals. Hug trees.',
+    parentId: 'abi04',
+  },
+  {
+    skillId: 'ski09',
+    name: 'Religion',
+    desc: 'Recognise deities. Understand holy rites.',
+    parentId: 'abi04',
+  },
+  {
+    skillId: 'ski10',
+    name: 'Animal Handling',
+    desc: 'Influence animals. Pet all of the dogs.',
+    parentId: 'abi05',
+  },
+  {
+    skillId: 'ski11',
+    name: 'Insight',
+    desc: 'Read people and situations. Detect lies.',
+    parentId: 'abi05',
+  },
+  {
+    skillId: 'ski12',
+    name: 'Medicine',
+    desc: 'Recognise symptoms. Diagnose diseases.',
+    parentId: 'abi05',
+  },
+  {
+    skillId: 'ski13',
+    name: 'Perception',
+    desc: 'Observe your environment. Spot hidden details.',
+    parentId: 'abi05',
+  },
+  {
+    skillId: 'ski14',
+    name: 'Survival',
+    desc: 'Stay alive in the wilds. Track prey.',
+    parentId: 'abi05',
+  },
+  {
+    skillId: 'ski15',
+    name: 'Deception',
+    desc: 'Lie and cheat. Manipulate the truth',
+    parentId: 'abi06',
+  },
+  {
+    skillId: 'ski16',
+    name: 'Intimidation',
+    desc: 'Be a bully. Threaten and induce fear.',
+    parentId: 'abi06',
+  },
+  {
+    skillId: 'ski17',
+    name: 'Performance',
+    desc: 'Entertain audiences. Command the stage.',
+    parentId: 'abi06',
+  },
+  {
+    skillId: 'ski18',
+    name: 'Persuasion',
+    desc: 'Turn on the charm. Coax and cajole.',
+    parentId: 'abi06',
+  },
+];
---
+++ src/models/ISkill.ts
@@ -0,0 +1,6 @@
+export interface ISkill {
+  skillId: string;
+  name: string;
+  desc: string;
+  parentId: string;
+}
